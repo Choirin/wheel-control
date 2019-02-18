563629c8fc92ff8144764cb50ec8d4b5b12489fb
Kohei Takada <khi.tkd@gmail.com>
2019-02-18 22:07:20 +0900

    wip

--- Src/time.c
+++ Src/time.c
@@ -3,6 +3,7 @@
 TIM_HandleTypeDef htim9;
 
 unsigned long int time_uppercount = 0;
+unsigned long int msec = 0;
 
 void InitializeTime(void)
 {
@@ -10,8 +11,18 @@ void InitializeTime(void)
   return;
 }
 
+
+void SetTimeRate(unsigned long int msec)
+{
+  rate_msec = msec;
+}
+
 void GetTime(void)
 {
-  printf("%lu:%d\r\n", time_uppercount, TIM9->CNT);
+  unsigned long int sec = time_uppercount;
+  unsigned long int nsec = TIM9->CNT;
+  nsec = (sec % 10 * 10000 + nsec) * 10000;
+  sec = sec / 10;
+  printf("%9lu.%09lu\r\n", sec, nsec);
   return;
 }
